d132bc6be2091e7fd0083df07c57c7872985ad4c
SoLayr <34119124+SoLayr@users.noreply.github.com>
2017-12-09 12:04:47 +0800

    Update Title.java

--- mutangyuan/jointitle/Title.java
+++ mutangyuan/jointitle/Title.java
@@ -1,1 +1,18 @@
+ package mutangyuan.jointitle;
+
+ import org.bukkit.configuration.file.FileConfiguration;
+/*  4:   */ import org.bukkit.entity.Player;
+/*  5:   */ import org.bukkit.event.EventHandler;
+/*  6:   */ import org.bukkit.event.Listener;
+/*  7:   */ import org.bukkit.event.player.PlayerJoinEvent;
+
+ public class Title
+   implements Listener
+ {
+   @EventHandler
+   {
+     Player p = evt.getPlayer();
+     p.sendTitle(Main.plugin.getConfig().getString("JoinTitle1").replace("&", "§"), Main.plugin.getConfig().getString("JoinTitle2").replace("&", "§"));
+   }
+ }
 
